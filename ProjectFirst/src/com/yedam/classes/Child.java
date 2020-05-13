c131a6bf1009c188946c843d9427160a6a1ec5e6
admin <ok2qkq@naver.com>
2020-05-13 12:51:41 +0900

    20.05.13

--- ProjectFirst/src/com/yedam/classes/Child.java
+++ ProjectFirst/src/com/yedam/classes/Child.java
@@ -9,6 +9,15 @@ public class Child extends Parent {
 	Child(String lastName, String firstName, String telNumber){
 		super(lastName, firstName, telNumber);	
 	}
+	Child(){
+		super();
+	}
+	void showInfo() {
+		Child child = new Child();
+		//child.getLastName();//default x
+		child.getFirstName();//protected
+		child.getTelNumber();//public
+	}
 	
 	
 }
