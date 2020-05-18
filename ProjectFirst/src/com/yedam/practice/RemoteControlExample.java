5de1a38c4b7a5c17931d9199e8b7f9dd578df03e
admin <ok2qkq@naver.com>
2020-05-18 16:57:01 +0900

    20.05.18 extra

--- ProjectFirst/src/com/yedam/practice/RemoteControlExample.java
+++ ProjectFirst/src/com/yedam/practice/RemoteControlExample.java
@@ -1,19 +1,31 @@
 package com.yedam.practice;
 
+import com.yedam.interfaces.Audio;
 import com.yedam.interfaces.RemoteControl;
+import com.yedam.interfaces.Television;
 
 public class RemoteControlExample {
 	public static void main(String[] args) {
-		RemoteControl rc = new RemoteControl() {
-			public void turnOn() {
-				System.out.println("장비를 실행합니다.");
-			}
-			public void turnOff() {
-				System.out.println("장비를 정지합니다. 어 안되잖아?");
-			}
-			public void setVolume(int volume) {
-				
-			}
-		};
+		
+		RemoteControl rc = null;
+		
+		rc = new Television();
+		rc.turnOn();
+		rc.turnOff();
+		
+		rc = new Audio();
+		rc.turnOn();
+		rc.turnOff();
+//		RemoteControl rc = new RemoteControl() {
+//			public void turnOn() {
+//				System.out.println("장비를 실행합니다.");
+//			}
+//			public void turnOff() {
+//				System.out.println("장비를 정지합니다. 어 안되잖아?");
+//			}
+//			public void setVolume(int volume) {
+//				
+//			}
+//		};
 	}
 }
