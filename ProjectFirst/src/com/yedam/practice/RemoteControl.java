3c56518ca53c8146c6cb47cd12535d70650f4102
admin <ok2qkq@naver.com>
2020-05-21 13:52:00 +0900

    20.05.21

---
+++ ProjectFirst/src/com/yedam/practice/RemoteControl.java
@@ -0,0 +1,18 @@
+package com.yedam.practice;
+
+public interface RemoteControl {
+	public int MAX_VOLUME = 10;
+	public int MIN_VOLUME = 0;
+	
+	public void turnOn();
+	public void turnOff();
+	public void setVolume(int volume);
+
+	default void setMute(boolean mute) {
+		if(mute) {
+			System.out.println("무음처리");
+		}else {
+			System.out.println("무음해제");
+		}
+	}
+}
